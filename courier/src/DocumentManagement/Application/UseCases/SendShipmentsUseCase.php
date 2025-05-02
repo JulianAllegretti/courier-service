1946c021dfbdc5ac3bb17efab6b066a502377e18
Julian Allegretti <julialegreti@outlook.com>
2025-05-02 16:02:34 -0500

    send centro operativo

--- courier/src/DocumentManagement/Application/UseCases/SendShipmentsUseCase.php
+++ courier/src/DocumentManagement/Application/UseCases/SendShipmentsUseCase.php
@@ -30,7 +30,8 @@ readonly class SendShipmentsUseCase implements ISendShipmentsUseCase
 
             $shipment = new SendShipments472Request(
                 $item['codigo_guia'], $this->clearText($item['nombre_completo']), $this->clearText($item['direccion']),
-                $codeDane->getName(), $codeDane->getDepto(), $item['num_radicado'], strtolower($item['prioridad']) == 'si' ? 'Urgente' : 'Normal'
+                $codeDane->getName(), $codeDane->getDepto(), $item['num_radicado'], strtolower($item['prioridad']) == 'si' ? 'Urgente' : 'Normal',
+                $codeDane->getCourier()
             );
 
             $request[] = $shipment;
